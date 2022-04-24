85c637c3964cd254fada4389400f352da7be4696
Jason Robinson <jmrobinsonma@gmail.com>
2022-04-24 11:21:24 -0400

    bashrc,hostname,yt

--- quick_setup.py
+++ quick_setup.py
@@ -1,15 +1,22 @@
 import os
 
+print()
 os.system("hostname")
 os.system("which python3")
+print()
 
 #distro = input("Ubuntu or Manjaro(u/m): ")
 
+change_hostname = input("Custom Hostname?(y/n): ")
+if change_hostname == "y":
+	print()
+	custom_hostname = input("Enter Hostname: ")
+	os.system(f"hostnamectl set-hostname {custom_hostname}")
+
 username = input("Username: ")
+print()
 privileged = input("Sudo(y/n): ")
-
-#password = input("Password: ")
-
+print()
 
 if privileged == "n":
 	privileged = False
@@ -22,27 +29,40 @@ else:
 	os.system(cmd)
 
 print(os.system("ls -l /home"))
+print()
 
+#password = input("Password: ")
 os.system(f"sudo passwd {username}")
 
 # custom bashrc
+os.system("cp ./bashrc_bk ~/.bashrc")
+
 
 # set dns/server ip
 
 os.system("sudo ufw enable")
 os.system("sudo ufw allow ssh")
 
-# install docker
-
 # install nmap
-
 # install tree
-
 # install net-tools
 os.system("sudo apt install -y net-tools tree nmap ranger git")
 
+# install docker
+#os.system("sudo apt-get install ca-certificates curl gnupg lsb-release")
+#os.system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg")
+#os.system("echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null")
+#os.system("sudo apt install docker.io")
+#os.system(f"usermod -aG docker {username}")
+
+# install YouTube downloader
+os.system("cp -r ./yt ~/")
+os.system("cp ~/yt/yt.sh ~/")
+
+print()
 os.system("which ranger")
 os.system("which net-tools")
 os.system("which tree")
 os.system("which nmap")
 os.system("which git")
+os.system("docker version")
